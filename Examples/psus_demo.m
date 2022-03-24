428e395abb7ec5732a94931b06b61b7e9cb11183
Peter Hristov <peter.hristov24@gmail.com>
2022-03-24 17:20:12 +0200

    Removed unnecessary use of cell of structs

--- Examples/psus_demo.m
+++ Examples/psus_demo.m
@@ -10,13 +10,13 @@ inpD = {'unif',[0,1]};
 
 %% Run 1 iter
 func = @(x)branin_uncert(x,1,mode); %Largest level of uncertainty
-[~,sOut{1}] = psus(func,d,t_star,N,p0,outD,inpD);
+[~,sOut] = psus(func,d,t_star,N,p0,outD,inpD);
     
 %% Run multiple iter's
 iter = [1,5,10,15,25,50,75,100,250:250:1000]';
 
 for i = 2:length(iter)
     func = @(x)branin_uncert(x,iter(i),mode); %Shrinking level of uncert
-    [~,sOut{i,1}] = psus(func,d,t_star,N,p0,outD,inpD);
+    [~,sOut(i,1)] = psus(func,d,t_star,N,p0,outD,inpD);
     disp(iter(i))
 end
